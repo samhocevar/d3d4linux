198ed8762663a85fde852bdc6acee3f350fe7f8e
Sam Hocevar <sam@hocevar.net>
2016-06-09 20:39:20 +0200

    Ensure simple.cpp compiles on Linux, too.

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 all: simple d3d4linux-server.exe
 
-CXXFLAGS += -I./include -std=c++11
+CXXFLAGS += -g -I./include -std=c++11
 
 d3d4linux-server.exe: server.cpp
 	x86_64-w64-mingw32-c++ $(CXXFLAGS) $^ -o $@
--- include/d3d4linux.h
+++ include/d3d4linux.h
@@ -1,10 +1,35 @@
 
-#include <cstdint>
-#include <cstddef>
+#include <cstdint> /* for uint32_t */
+#include <cstddef> /* for size_t */
+#include <cstdio> /* for FILE */
+
+#include <string> /* for std::string */
+
+/*
+ * Types and macros that come from Windows
+ */
+
+typedef int HRESULT;
+typedef int HMODULE;
+
+#define FAILED(x) ((x) != 0)
 
 /*
  * Types and macros that mimic some of the ones in D3D
  */
+
+#define D3DCOMPILE_DEBUG                          0x0001
+#define D3DCOMPILE_SKIP_VALIDATION                0x0002
+#define D3DCOMPILE_SKIP_OPTIMIZATION              0x0004
+#define D3DCOMPILE_PACK_MATRIX_ROW_MAJOR          0x0008
+#define D3DCOMPILE_AVOID_FLOW_CONTROL             0x0200
+#define D3DCOMPILE_PREFER_FLOW_CONTROL            0x0400
+#define D3DCOMPILE_ENABLE_BACKWARDS_COMPATIBILITY 0x1000
+
+#define D3DCOMPILE_OPTIMIZATION_LEVEL0 0x4000
+#define D3DCOMPILE_OPTIMIZATION_LEVEL1 0x0000
+#define D3DCOMPILE_OPTIMIZATION_LEVEL2 0xc000
+#define D3DCOMPILE_OPTIMIZATION_LEVEL3 0x8000
 
 #define D3D10_SHADER_DEBUG                          0x0001
 #define D3D10_SHADER_SKIP_VALIDATION                0x0002
@@ -14,6 +39,7 @@
 #define D3D10_SHADER_PREFER_FLOW_CONTROL            0x0400
 #define D3D10_SHADER_ENABLE_BACKWARDS_COMPATIBILITY 0x1000
 
+#define D3D10_SHADER_OPTIMIZATION_LEVEL0 0x4000
 #define D3D10_SHADER_OPTIMIZATION_LEVEL1 0x0000
 #define D3D10_SHADER_OPTIMIZATION_LEVEL2 0xc000
 #define D3D10_SHADER_OPTIMIZATION_LEVEL3 0x8000
@@ -25,8 +51,41 @@ struct D3D_SHADER_MACRO
 };
 
 // FIXME
-typedef void ID3DInclude;
-typedef void ID3DBlob;
+struct ID3DInclude
+{
+};
+
+struct ID3DBlob
+{
+    char const *GetBufferPointer() const
+    {
+        return contents.c_str();
+    }
+
+    size_t GetBufferSize() const
+    {
+        return contents.size();
+    }
+
+    void Release()
+    {
+        delete this;
+    }
+
+    std::string contents;
+};
+
+typedef long (*pD3DCompile)(void const *pSrcData,
+                            size_t SrcDataSize,
+                            char const *pFileName,
+                            D3D_SHADER_MACRO const *pDefines,
+                            ID3DInclude *pInclude,
+                            char const *pEntrypoint,
+                            char const *pTarget,
+                            uint32_t Flags1,
+                            uint32_t Flags2,
+                            ID3DBlob **ppCode,
+                            ID3DBlob **ppErrorMsgs);
 
 /*
  * Helper struct for compilation
@@ -51,7 +110,7 @@ struct d3d4linux
             return -1;
 
         pclose(p);
-        return 0;
+        return -1;
     }
 };
 
@@ -71,5 +130,19 @@ long D3DCompile(void const *pSrcData,
     return d3d4linux::compile(pSrcData, SrcDataSize, pFileName, pDefines,
                               pInclude, pEntrypoint, pTarget, Flags1,
                               Flags2, ppCode, ppErrorMsgs);
+}
+
+/*
+ * Helper functions for Windows
+ */
+
+static inline HMODULE LoadLibrary(char const *name)
+{
+    return 0;
+}
+
+static void *GetProcAddress(HMODULE, char const *name)
+{
+    return (void *)&d3d4linux::compile;
 }
 
